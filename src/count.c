cd35427fdef2681598ff8b552cc498387aa9ec0d
ian <ian@gregson.me>
2022-11-03 22:56:20 +0000

    Add some copy and counting

---
+++ src/count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* count the characters from stdin */
+
+int main() {
+
+  double nc;
+
+  // nc = 0;
+
+  // while (getchar() != EOF) {
+  //   ++nc;
+  // }
+
+  for (nc = 0; getchar() != EOF; ++nc) {
+    
+  }
+
+  printf("%.0f\n", nc);
+}
--- src/farenheit_celsius.c
+++ src/farenheit_celsius.c
@@ -3,24 +3,23 @@
 /* print Farenheit-Celsius Table 
    for fahr = 0, 20, ..., 300 */
 
+#define LOWER 0   /* Temperature floor */
+#define UPPER 300 /* Temperature ceiling */
+#define STEP  20  /* Temperature increment */
+
 int main()
 {
   float fahr, celsius;
-  int lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+  fahr = LOWER;
 
-  fahr = lower;
-
-  while (fahr <= upper) {
+  while (fahr <= UPPER) {
     celsius = (5.0/9.0) * (fahr-32);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
 
-  for (fahr = 0; fahr <= upper; fahr = fahr + 20) {
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - 20) {
     celsius = (5.0/9.0) * (fahr-32);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
   }
